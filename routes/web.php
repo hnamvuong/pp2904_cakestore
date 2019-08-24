3504824599c2cc8e79e0c7ceec20938665098250
hnamvuong <hnam.vuong@gmail.com>
2019-08-24 16:56:02 +0700

    feat(server): view data homepage

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('index', [
+Route::get('/', [
 	'as' => 'trang-chu',
 	'uses' => 'PageController@getIndex'
 ]);
